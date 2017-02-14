4e5764c40740ea238022ac0766edd28c9d8b49a8
Michael Connors <michaeljconnors14@gmail.com>
2017-02-14 11:55:35 -0800

    store data of appropiate type, release 2

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -4,21 +4,30 @@ hamster_name = gets.chomp
 
 puts "Volume level (1-10 scale)"
 
-volume_scale = gets.chomp
+volume_scale = gets.chomp.to_i
 
 puts "fur color?"
 
 fur_color = gets.chomp
 
-puts "good canidate for adoption?"
+puts "good candidate for adoption (y/n)?"
 
-canidate = gets.chomp
+candidate = gets.chomp
+
+if candidate == "y"
+	good = true
+else
+	good = false
+end
 
 puts "estimated age?"
-
 
 age = gets.chomp
 
 if age == ""
 	age = nil
+else
+	age = age.to_f
 end
+
+puts age

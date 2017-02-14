13efaab7571d3aac94d172d5ec9e4cfdd8451ebb
Michael Connors <michaeljconnors14@gmail.com>
2017-02-14 11:37:42 -0800

    add inputs

---
+++ ruby/hamsters.rb
@@ -0,0 +1,24 @@
+puts "Hamster name?"
+
+hamster_name = gets.chomp
+
+puts "Volume level (1-10 scale)"
+
+volume_scale = gets.chomp
+
+puts "fur color?"
+
+fur_color = gets.chomp
+
+puts "good canidate for adoption?"
+
+canidate = gets.chomp
+
+puts "estimated age?"
+
+
+age = gets.chomp
+
+if age == ""
+	age = nil
+end

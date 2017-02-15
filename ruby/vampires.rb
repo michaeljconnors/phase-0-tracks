9ab30213df9c73b957786ba50d355eb4f167d6bf
Michael Connors <michaeljconnors14@gmail.com>
2017-02-15 11:57:45 -0800

    release 1, add questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,19 @@
+puts "What is your name?"
+
+your_name = gets.chomp
+
+puts "How old are you?"
+
+age = gets.chomp
+
+puts "What year were you born?"
+
+year_born = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+
+insurance = gets.chomp

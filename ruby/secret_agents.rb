f70bb7983d0d9e82808e1109b90fbb1cff6fc6f8
Michael Connors <michaeljconnors14@gmail.com>
2017-02-16 16:28:51 -0800

    pheudocode

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,13 @@
+#encrypt
+#set index to zero
+# name variable encrypt_code and include 
+# after assigning variable use .next to advance one letter forward
+
+#decrypt
+#set index to 0
+#create loop that grabs the first letter
+#find that letter in the alphabet and assign the number to a variable
+#subract the number one from the variable and then pull the new number from the alphabet
+#add letter to the word then loop
+
+

cafd89a6e98db0691ff5180ed38984b2ad8ad602
Michael Connors <michaeljconnors14@gmail.com>
2017-02-18 14:17:13 -0800

    add more 'suspects'

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,59 +4,62 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
 
 p "iNvEsTiGaTiOn".swapcase
 
-# "zom".<???>
-# => “zoom”
+# => “InVeStIgAtIoN”
+
 
 p "zom".insert(1, 'o')
 
+p "zom".gsub(/[o]/, 'oo')
 
-# "enhance".<???>
-# => "    enhance    "
+# => “zoom”
 
 p "enhance".center(4)
 
+# => "    enhance    "
+
 # "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+
 
 p "Stop! You’re under arrest!".upcase
 
+# => "STOP! YOU’RE UNDER ARREST!"
+
 # "the usual".<???>
-#=> "the usual suspects"
+
 
 p "the usual".insert(9, " suspects")
 
+p "the usual".gsub(/[l]/, 'l suspects')
+
 # " suspects".<???>
-# => "the usual suspects"
+
 
 p " suspects".insert(0, "the usual")
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+p " suspects".gsub(/[ ]/, 'the usual ')
+
+# => "the usual suspects"
 
 p "The case of the disappearing last letter"[0...-1]
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+# => "The case of the disappearing last lette"
 
 p "The mystery of the missing first letter"[1...40]
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+# => "he mystery of the missing first letter"
 
 p "Elementary,    my   dear        Watson!".squeeze(" ")
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# => "Elementary, my dear Watson!"
 
 p "z".bytes
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+# => 122 
+# the significance of the number is it's the ASCII value of the letter.
+
 
 p "How many times does the letter 'a' appear in this string?".count "a"
+# => 4

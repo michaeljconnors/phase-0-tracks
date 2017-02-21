fddb15a01dec378fc78cc57ea3720d1ee56fa06a
Michael Connors <michaeljconnors14@gmail.com>
2017-02-21 10:45:31 -0800

    add psuedo code

---
+++ ruby/hashes.rb
@@ -0,0 +1,9 @@
+# Instruct interior designer to input the following information
+# Allow for input of clients name
+# Allow for input of clients age
+# Allow for input of clients number of children
+# Allow for input of clients decor theme
+# Allow for input of clients favorite colors
+# Allow for input of clients least favorite color
+# Allow for input of clients oldest childs age
+# Allow for input of clients youngest childs age

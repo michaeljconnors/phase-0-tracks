c7edbf1630514254043b18aa7e6f46285d50da72
Michael Connors <michaeljconnors14@gmail.com>
2017-02-22 19:02:13 -0800

    add own block

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -2,3 +2,5 @@ def block
   puts "DBC is super fun!"
   yield("Glenna", "Emmanuel", "Eli")
 end
+
+block { |teacher1, teacher2, teacher3| puts "Our teachers #{teacher1}, #{teacher2} and #{teacher3} are cool." }

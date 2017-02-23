b1947df28c45e0f7a5d0678d3eb77401da90063d
Michael Connors <michaeljconnors14@gmail.com>
2017-02-23 11:15:28 -0800

    build nested structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,33 @@
+football_team = {
+  offense: {
+    coach: 'Gary True',
+    trench_players: {
+      center: "Bob",
+      guard: "Allen",
+      tackle: "Mike"
+    },
+    skilled_players: {
+      quarterback: "Tom",
+      running_Back: "Woody",
+      wide_Reciever: "Stryder"
+    },
+    strengths: ["play_action", "running"]
+    
+  },
+  defense: {
+    coach: 'Bill Riot',
+    trench_players: {
+      tackle: "Sam",
+      defense_End: "Marcus",
+    },
+    skilled_players: {
+      linebacker: "Clay",
+      safety: "Rob",
+      corner: "John"
+    },
+    strengths: ["pressure", "coverage"]
+    
+  }
+}
+
+p football_team

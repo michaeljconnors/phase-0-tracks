c3bd94bd877e7c4bf13003b24c4c33e6764ed8f0
Michael Connors <michaeljconnors14@gmail.com>
2017-02-22 19:04:27 -0800

    complete release 1, note this was done on another branch originally but now adding steps to new branch

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -4,3 +4,54 @@ def block
 end
 
 block { |teacher1, teacher2, teacher3| puts "Our teachers #{teacher1}, #{teacher2} and #{teacher3} are cool." }
+
+#Release 1 'array'
+
+oceans = ["pacific", "atlantic", "indian", "artic", "southern"]
+oceans_capitalize = []
+
+#each
+
+p oceans
+
+oceans.each do |oceans|
+  oceans_capitalize << oceans.capitalize
+end
+
+puts "Oceans of the world are:"
+p oceans_capitalize
+
+#map
+
+p oceans
+
+oceans.map! do |oceans|
+  puts oceans
+  oceans.upcase
+end
+
+p oceans
+
+puts "Oceans of the world are:"
+p oceans_capitalize
+
+#Release 1 'hash'
+
+world_oceans = {
+  "ocean_1" => 'pacific', 
+  "ocean_2" => 'atlantic', 
+  "ocean_3" => 'indian', 
+  "ocean_4" => 'artic', 
+  "ocean_5" => 'southern'
+  
+}
+
+p world_oceans
+
+#each
+
+world_oceans.each do |num, ocean|
+  puts "#{num.capitalize} is the #{ocean}."
+end
+
+p world_oceans

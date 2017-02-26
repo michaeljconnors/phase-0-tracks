f373b1a9486de4620a32def67b85f6724599b0e4
Michael Connors <michaeljconnors14@gmail.com>
2017-02-25 16:22:22 -0800

    write characteristics of puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Fur color: varies
+# Friendly: yes
+# Eye count: 2
+# Leg count: 4
+# Tail: yes
+# Name: varies
+# Carnivore: yes
+
+# BEHAVIOR -----------------------
+# Pant
+# Wag tail
+# Jump
+# Stick tounge out
+# Fetch
+# Run

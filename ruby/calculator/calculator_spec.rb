018242ce75450802bcb42f18130a85348e252a6f
Michael Connors <michaeljconnors14@gmail.com>
2017-03-01 08:14:48 -0800

    complete it block, 4 examples

--- ruby/calculator/calculator_spec.rb
+++ ruby/calculator/calculator_spec.rb
@@ -11,8 +11,8 @@
 require_relative 'calculator'
 
 describe Calculator do
-  let(:calculator) { Calculator.new }
 
+  let(:calculator) { Calculator.new }
   it "adds two integers" do
     expect(calculator.add(3,4)).to eq 7
   end
@@ -24,4 +24,9 @@ describe Calculator do
   it "multiplies two integers" do
     expect(calculator.multiply(2,3)).to eq 6
   end
+
+  it "divide two integers" do
+   expect(calculator.divide(12,2)).to eq 6
+  end
 end
+

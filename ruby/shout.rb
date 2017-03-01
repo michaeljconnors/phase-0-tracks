451ba7474fdf36bcd5ff4e05f6b5eefccd3613b8
Michael Connors <michaeljconnors14@gmail.com>
2017-02-28 20:28:07 -0800

    add mix in, add driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,35 @@
+#module Shout
+#  def self.yell_angrily(words)
+#    words + "!!!" + " :("
+#  end
+#  def self.yelling_happily(words)
+#  	words + "!!!" + " :)"
+#  end
+#end
+
+#p Shout.yell_angrily("Don't you dare touch that")
+
+#p Shout.yelling_happily("I can't believe I won the lottery")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-  def self.yelling_happily(words)
-  	words + "!!!" + " :)"
-  end
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+	def yelling_happily(words)
+		words + "!!!" + " :)"
+	end
 end
 
-p Shout.yell_angrily("Don't you dare touch that")
+class Coach
+	include Shout
+end
 
-p Shout.yelling_happily("I can't believe I won the lotery")
+class Fan 
+	include Shout
+end
+
+coach = Coach.new
+coach.yell_angrily("How many times have I told you")
+
+fan = Fan.new
+fan.yelling_happily("Go! Go! Go! Touchdown")

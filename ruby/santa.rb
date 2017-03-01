9c6dd89a844f8b0aa83f9c5c20b8bf00ff694b82
Michael Connors <michaeljconnors14@gmail.com>
2017-02-28 18:19:18 -0800

    add setter methods and getter

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,9 +16,38 @@ class Santa
 		puts "That was a good chocolate chip cookie!"
 	end
 
+#getter methods
+  def age
+    @age
+  end
+
+  def enthnicity
+    @enthnicity
+  end
+  
+	#setter methods
+	def celebrate_birthday=(age)
+  	@age = age + 1
+	end
+
+  def get_mad_at=(reindeer_ranking)
+   @reindeer_ranking.delete(reindeer_ranking)
+   @reindeer_ranking << reindeer_ranking
+   p @reindeer_ranking
+  end
+  
+  def gender=(gender)
+    @gender = gender
+  end
+
 end
 
-santa = Santa.new("female", "male")
-p santas
+santa = Santa.new("female", "Scandinavian")
+p santa
 p santa.eat_milk_and_cookies
 p santa.speak
+p santa.celebrate_birthday = 1
+santa.get_mad_at = "Vixen"
+p gender = "male"
+p santa.age
+p santa.enthnicity

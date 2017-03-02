2a880bf72290724c04a30fee5a8b7614fce4a0a7
Michael Connors <michaeljconnors14@gmail.com>
2017-03-01 21:04:10 -0800

    first go at it, created a couple of calls with verification

---
+++ ruby/game/game_spec.rb
@@ -0,0 +1,29 @@
+# our game should ...
+# have one user enter a hidden word.
+# have the hidden word be converted into an array
+# set up a method that prints the length of the letters of the word so they return "_ _ "
+# multiply two integers and return an integer
+
+# RSpec
+# 'describe' block for each group of tests  
+  # 'it' block for each individual test
+    # expect(<YOUR CODE>).to eq <RESULT>
+
+require_relative 'game'
+
+describe Game do
+
+  let(:game) { Game.new("unicorn") }
+
+ it "stores the list items given on initialization" do
+    expect(game.initial_feedback).to be == "_  _  _  _  _  _  _"
+  end
+
+it "stores the list items given on initialization" do
+    expect(game.update_feedback("u")).to be == "u______"
+  end
+end
+
+
+
+

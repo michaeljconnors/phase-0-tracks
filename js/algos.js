d2a4479d264c7569d7852a97fdd97a2638163301
Michael Connors <michaeljconnors14@gmail.com>
2017-03-07 14:39:41 -0600

    release 0 complete, add function that takes array and returns largest value

--- js/algos.js
+++ js/algos.js
@@ -4,9 +4,25 @@
 //   create another empty array called length
 //   create largest word vairable
 //   	put array of words through loop that determines length length 
-//			run IF statement thay sets it as the largest word vairable if greater than the last variable
+//			run IF statement that sets it as the largest word vairable if greater than the last variable
 // output: returns largest value in array
 
+var phrase = ["long phrase","longest phrase","longer phrase", "oh no you didn't", "the usc trojans"];
+var colors = ["blue", "green", "pink", "orange",];
+var names = ["Ed", "Seabiscuit", "Black Beauty", "Oreo"];
 
 
+function longest(array){
+  var largestWord = "";
+  for (var i = 0; i < array.length; i++) {
+	if (largestWord.length < array[i].length) {
+	  largestWord = array[i];
+	}
+	} 
+	console.log(largestWord);
+}
 
+
+longest(phrase);
+longest(colors);
+longest(names);

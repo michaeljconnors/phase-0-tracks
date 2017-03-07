d3152ab413dc871db9ba15a4176ca63003e8567c
Michael Connors <michaeljconnors14@gmail.com>
2017-03-07 15:52:55 -0600

    more phseodo with a little more code

--- js/algos.js
+++ js/algos.js
@@ -13,11 +13,11 @@ var names = ["Ed", "Seabiscuit", "Black Beauty", "Oreo"];
 
 
 function longest(array){
-  var largestWord = "";
-  for (var i = 0; i < array.length; i++) {
-	if (largestWord.length < array[i].length) {
-	  largestWord = array[i];
-	}
+	var largestWord = "";
+	for (var i = 0; i < array.length; i++) {
+		if (largestWord.length < array[i].length) {
+			largestWord = array[i];
+		}
 	} 
 	console.log(largestWord);
 }
@@ -26,3 +26,28 @@ function longest(array){
 longest(phrase);
 longest(colors);
 longest(names);
+
+
+
+// function that takes two objects and checks to see if they share one key value
+// input: two objects
+// steps: loop through one objects keys and values and see if any value is returned
+//    IF the value is greater than 0 then return true
+//       Else return false
+// output: return either true or false
+
+var eyes = {ed: "blue", becky: "hazel", mari: "brown"};
+var eyes_two = {fred: "red", beth: "blue", mari: "brown"};
+
+console.log(eyes_two.beth);
+console.log(eyes_two);
+
+for (var people in eyes) {
+  var compare = eyes[people] +": "+ people;
+  console.log(compare);
+}
+
+for (var people in eyes_two) {
+  var compare = eyes_two[people] +": "+ people;
+  console.log(compare);
+}

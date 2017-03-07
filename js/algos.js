ae337bdf8f5f60c7cdf49b023801665a476502ad
Michael Connors <michaeljconnors14@gmail.com>
2017-03-07 14:29:44 -0600

    phuedo code

---
+++ js/algos.js
@@ -0,0 +1,12 @@
+// function to take array of words and return the longest word
+// input: array of words
+// steps:
+//   create another empty array called length
+//   create largest word vairable
+//   	put array of words through loop that determines length length 
+//			run IF statement thay sets it as the largest word vairable if greater than the last variable
+// output: returns largest value in array
+
+
+
+

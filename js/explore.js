5b51c0bc22f687eca4f8eda69bb07037339f0901
Michael Connors <michaeljconnors14@gmail.com>
2017-03-07 09:35:30 -0600

    add phseodo code

---
+++ js/explore.js
@@ -0,0 +1,15 @@
+//Ruby has lots of handy functions like .reverse, but in JavaScript, we mostly have to write our own. 
+//In comments, pseudocode a function that takes a string as a parameter and reverses the 
+//string. reverse("hello"), for example, should return "olleh". This isn't a task specific 
+//to JavaScript -- your pseudocode should be in plain English and use phrases like "for each" 
+//instead of JavaScript-specific terms. Remember that you can add strings in JavaScript, so '' + 'a' would 
+//result in the string 'a'. Take your time. This is the most valuable skill in programming: being able to 
+//think through a problem logically.
+
+// method to take string and reverse order
+// input: variable string
+// steps: 
+//    take string and for each letter 
+//      assign to a new variable by adding the letters together
+// output: return the word in reverse order
+

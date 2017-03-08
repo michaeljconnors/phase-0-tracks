78ca29fcae3298f0d0832690d72d4fa5f09a9eb2
Michael Connors <michaeljconnors14@gmail.com>
2017-03-07 22:45:22 -0600

    require relative, analyze state data,comment on each method, implement new feature, refactor

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,26 +4,32 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# Require relative allows the program to access data in a local file,
+# Require alone allows the program to access programs and code outside Rubys normal functionality.
 require_relative 'state_data'
 
 class VirusPredictor
 
+# creates a new instance of that class, accepting inputs that are then put into attributes that are accessabile throughout the class.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+# calling two other methods and running them.
+
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+# its taking in three inputs and comparing to five paremeters that takes in a states population density and then based off where it falls multiplies by a factor that then returns the number of deaths.
+# lastly it then prints out a statement using the states name with the newly calculated number of deaths
+
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -41,7 +47,10 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+# taking the population density and the state as inputs then comparing the population density to a set of five paremeters based off the higher density the faster the speed of the virus will spread.
+# prints a statement declaring the rate of speed of disease in months.
+
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -82,6 +91,10 @@ california.virus_effects
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
 
+STATE_DATA.each do |state, data|
+state = VirusPredictor.new(state, data[:population_density], data[:population])
+state.virus_effects
+end
 
 #=======================================================================
 # Reflection Section
